5637dec24340b908d01758c14662ff25b6ba84b1
Adrian Roata <adi.roata@gmail.com>
2020-01-15 00:28:38 +0200

    Create nested_telnet.py

---
+++ nested_telnet.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import getpass
+import sys
+import telnetlib
+#import time
+import os
+#import itertools
+
+host_pe = raw_input ("Introduceti adresa PE: ")
+user_pe = raw_input("Introduceti username PE: ")
+password_pe = getpass.getpass()
+
+user_cpe = raw_input("Introduceti username CPE: ")
+password_cpe = getpass.getpass()
+
+f=open("lista-adrese-cpe", "r")
+
+for line in f:
+    try:
+        print("Se acceseaza CPE-ul " + str(line.strip()))
+
+#   Conectarea pe PE
+        telnet  = telnetlib.Telnet(host_pe)
+        telnet.read_until("Username: ", 5)
+        telnet.write(user_pe + "\r")
+        telnet.read_until("Password: ", 5)
+        telnet.write(password_pe + "\r")
+
+#   Conectarea pe CPE
+        telnet.write("telnet " + str(line.strip()) + " /vrf nume_vrf\r")
+        telnet.read_until("username: ", 5)
+        telnet.write(user_cpe + "\r")
+        telnet.read_until("password: ", 5)
+        telnet.write(password_cpe + "\r")
+        telnet.write("\n")
+        telnet.write("display interface description\r")
+        telnet.write("quit\n")
+
+#   Se salveaza output-ul
+        readoutput = telnet.read_until("quit", 10)
+        saveoutput = open("output_telnet", "ab")
+        saveoutput.write(readoutput)
+        saveoutput.close
+        print readoutput
+    except:
+        print("Hostul " + str(line.strip()) + " e down.")
