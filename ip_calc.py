9a12f86bd820e0d2681c89329f73fa311d28b54f
Adrian Roata <adi.roata@gmail.com>
2020-01-16 08:25:34 +0200

    Update ip_calc.py

--- ip_calc.py
+++ ip_calc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import ipaddress
+import os
 import csv
 
 ipadd = raw_input('Introduceti adresa (A.B.C.D/nm): ')
@@ -20,19 +21,25 @@ print('-------------------------------------------------\n')
 print('Numarul de hosturi este: ' + str(net.num_addresses))
 print('-------------------------------------------------\n')
 
-y=str('y')
+p=str('p')
+ps=str('ps')
 s=str('s')
+
 n = ipaddress.ip_network(net)
 
-answer1 = raw_input('Doriti sa  vedeti hosturile utilizabile ?\n (y/n, Save in silent mode: s)   ')
+answer1 = raw_input('List hosts ?\n\nJust print: p\nPrint&Save: ps\nJust save: s\nAbort: a\n\nYour choice:   ')
 
-if answer1 == y:
-	print("Lista hosturilor va fi salvata in fisierul hosts.csv")
+if answer1 == p:
 	for ip in n.hosts():
-		with open("hosts.csv", "a") as output1:
-			writer1 = csv.writer(output1)
-			writer1.writerow([ip,str(host.netmask)])
 		print ip
+
+elif answer1 == ps:
+	print("Lista hosturilor va fi salvata in fisierul hosts.csv")
+        for ip in n.hosts():
+                with open("hosts.csv", "a") as output1:
+                        writer1 = csv.writer(output1)
+                        writer1.writerow([ip,str(host.netmask)])
+                print ip
 
 elif answer1 == s:
 	print("Lista hosturilor va fi salvata in fisierul hosts.csv")
@@ -46,16 +53,20 @@ else:
 
 print('-------------------------------------------------\n')
 
-answer2 = raw_input('Doriti sa subnetizati ?\n (y/n, Save in silent mode: s)   ')
+answer2 = raw_input('List subnets ?\n\nJust print: p\nPrint&Save: ps\nJust save: s\nAbort: a\n\nYour choice:   ')
 
-if answer2 == y:
+if answer2 == p:
+        newcidr = raw_input('introduceti noul CIRD: ')
+        for nets in n.subnets(new_prefix=int(newcidr)):
+                print nets
+
+elif answer2 == ps:
 	newcidr = raw_input('introduceti noul CIRD: ')
 	print('Lista subneturilor va fi salvata in fisierul subnets.csv')
 	for nets in n.subnets(new_prefix=int(newcidr)):
 		with open("subnets.csv", "a") as output2:
                         writer2 = csv.writer(output2)
 			writer2.writerow([nets,])
-#                        writer2.writerow([nets, "/"+str(newcidr)])
 		print nets
 
 elif answer2 == s:
@@ -68,3 +79,5 @@ elif answer2 == s:
 
 else:
 	print("That's all for now. Bye!")
+
+# Writen by Adrian Roata | adi.roata@gmail.com
