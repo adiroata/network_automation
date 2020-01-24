5a165d4f61e9c4940f7dd4505c40f8f188045dfb
Adrian Roata <adi.roata@gmail.com>
2020-01-24 16:08:19 +0200

    Update snmp_get.py

--- snmp_get.py
+++ snmp_get.py
@@ -26,5 +26,8 @@ for n in range(4):
 		index=item.value
 		sys_if = session.get('.1.3.6.1.2.1.2.2.1.2.' + index)
 		sys_if_desc = session.get('.1.3.6.1.2.1.31.1.1.1.18.' + index)
-		print(str(sys_id.value) + ' | ' + str(sys_if.value) + ' | ' + str(sys_if_desc.value)  + ' | ' + str(sys_ipaddr))
+		print(str(sys_id.value) + ','
+		      + str(sys_if.value) + ','
+		      + str(sys_if_desc.value)  + ','
+		      + str(sys_ipaddr))
 		
