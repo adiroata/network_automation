8ee16c1ca16733bae01ef5187c821d58cbf17b3a
Adrian Roata <adi.roata@gmail.com>
2020-02-15 14:51:49 +0200

    new file:   meraki_api_get.py 	new file:   meraki_api_put.py

---
+++ meraki_api_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import requests
+import json
+
+url = "https://api.meraki.com/api/v0/networks/{{networkId}}/clients"
+
+payload = {}
+headers = {
+  'Accept': '*/*',
+  'X-Cisco-Meraki-API-Key': '{{Meraki_API_key}}',
+  'User-Agent': 'PostmanRuntime/7.22.0',
+  'Cache-Control': 'no-cache',
+  'Postman-Token': '{{Token}}',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Referer': 'https://api.meraki.com/api/v0/networks/{{networkId}}/clients',
+  'Connection': 'keep-alive'
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+jsonfile = json.loads(response.text.encode('utf8'))
+
+#print(jsonfile)
+
+for dictionary in jsonfile:
+    keys = ['description','manufacturer','mac','ip','lastSeen','ssid','status']
+    print('')
+    for key in keys:
+        output = dictionary.get(key)
+        print str(key),':',str(output)
+print('')
---
+++ meraki_api_put.py
@@ -0,0 +1,54 @@
+import requests
+
+files = [
+
+]
+headers = {
+  'Accept': '*/*',
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'X-Cisco-Meraki-API-Key': '{{Meraki_API_key}}',
+  'User-Agent': 'PostmanRuntime/7.22.0',
+  'Cache-Control': 'no-cache',
+  'Postman-Token': '{{Token}}',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Content-Length': '273',
+  'Connection': 'keep-alive'
+}
+
+choice = raw_input('Configure ? (yes/no)\n')
+
+networks = ['{{networkId}}', '{{networkId}}']
+
+if choice == 'yes':
+    for i in networks:
+        preurl = "https://api.meraki.com/api/v0/networks/"+str(i)+"/ssids/"
+        for n in range (10, 15):
+            url = preurl+str(n)
+            payload = {'number': n,
+            'name': 'Python SSID '+str(n),
+            'authMode': 'psk',
+            'psk': 'pythoniscool',
+            'encryptionMode': 'wpa',
+            'wpaEncryptionMode': 'WPA2 only',
+            'ipAssignmentMode': 'NAT mode',
+            'enabled': 'true'}
+            response = requests.request("PUT", url, headers=headers, data = payload, files = files)
+
+            print(response.text.encode('utf8'))
+
+elif choice == 'no':
+    for i in networks:
+        preurl = "https://api.meraki.com/api/v0/networks/"+str(i)+"/ssids/"
+        for n in range (10, 15):
+            url = preurl+str(n)
+            payload = {'number': n,
+            'name': 'Unconfigured SSID '+str(n),
+            'authMode': 'open',
+            'ipAssignmentMode': 'NAT mode',
+            'enabled': 'false'}
+            response = requests.request("PUT", url, headers=headers, data = payload, files = files)
+
+            print(response.text.encode('utf8'))
+
+else:
+    print 'Invalid option.'
